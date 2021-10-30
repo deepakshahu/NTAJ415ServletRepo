4e15f2f2c908da1f712594dff2e29eeb974ba304
deepa <deepa@DESKTOP-AJQTNNR>
2021-10-30 18:37:32 +0530

    DynamicForms

---
+++ DynamicForms/src/main/java/com/nit/servlet/FirstServlet.java
@@ -0,0 +1,55 @@
+package com.nit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/firsturl")
+public class FirstServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		//Get PrintWriter
+		PrintWriter pw = res.getWriter();
+		
+		//Set Response content type
+		res.setContentType("text/html");
+		
+		//Read form1/request1 data
+		String name = req.getParameter("pname");
+		String fname = req.getParameter("fname");
+		String address = req.getParameter("addrs");
+		String ms = req.getParameter("ms");
+		
+		//Generate form2 based on marital status value
+		if(ms.equalsIgnoreCase("married")) {
+			pw.println("<form action='secondurl' method='POST'>");
+			pw.println("<table border='1' bgcolor='pink' align='center'>");
+			pw.println("<tr><td>Spouse Name: </td><td><input type='text' name='f2t1'></td></tr>");
+			pw.println("<tr><td>No of Kids: </td><td><input type='text' name='f2t2'></td></tr>");
+			pw.println("<tr><td><input type='submit' value='Register'></td><td><input type='reset' value='cancel'></td></tr>");
+			pw.println("</table></form>");
+		}
+		else {
+			pw.println("<form action='secondurl' method='POST'>");
+			pw.println("<table border='1' bgcolor='pink' align='center'>");
+			pw.println("<tr><td>Why do you want to marry: </td><td><input type='text' name='f2t1'></td></tr>");
+			pw.println("<tr><td>When do you want to marry: </td><td><input type='text' name='f2t2'></td></tr>");
+			pw.println("<tr><td><input type='submit' value='Register'></td><td><input type='reset' value='cancel'></td></tr>");
+			pw.println("</table></form>");
+		}
+		
+		//close stream
+		pw.close();
+	}//doGet(-,-)
+	
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req,res);
+	}//doPost(-,-)
+}//class
---
+++ DynamicForms/src/main/java/com/nit/servlet/SecondServlet.java
@@ -0,0 +1,51 @@
+package com.nit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/secondurl")
+public class SecondServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		//Get PrintWriter
+		PrintWriter pw = res.getWriter();
+
+		//Set Response content type
+		res.setContentType("text/html");
+
+		//Read form1/request1 data
+		String name = req.getParameter("pname");
+		String fname = req.getParameter("fname");
+		String address = req.getParameter("addrs");
+		String ms = req.getParameter("ms");
+
+		//Read form2/request2 data
+		String f2val1 = req.getParameter("f2t1");
+		String f2val2 = req.getParameter("f2t2");
+		
+		//Display form1/request1 data
+		pw.println("<table border='1' bgcolor='yellow' align='center'>");
+		pw.println("<tr><td>Form1/Request1 Data:</td><td>"+name+"</td><td>"+fname+"</td><td>"+address+"</td><td>"+ms+"</td>");
+		pw.println("<tr><td>Form2/Request2 Data:</td><td>"+f2val1+"</td><td>"+f2val2+"</td>");
+		pw.println("</table>");
+		
+		//Add home hyperlink
+		pw.println("<h1 style='text-align:center'><a href='person_details.html'>Home</a></h1>");
+		
+		//Close stream
+		pw.close();
+	}//doGet(-,-)
+
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		doGet(req,res);
+	}//doPost(-,-)
+}//class
